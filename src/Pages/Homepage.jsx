3e4efdb9345e3ec9bbfc939e7fbafba001f56d05
odunayo300 <isaacodunayo300@gmail.com>
2025-05-22 20:22:34 -0700

    v4 push to production to correct setissignedup

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -51,12 +51,13 @@ const Homepage = () => {
     const [selectedCrops, setSelectedCrops] = useState([]); // Selected crops
     const [alertMessage, setAlertMessage] = useState(null); // Alert message
     const [alertType, setAlertType] = useState('success'); // Alert type ('success' or 'error')
-    const [isSignedUp, _setIsSignedUp] = useState(true); // Placeholder for sign-up status
+    const [isSignedUp, setIsSignedUp] = useState(true); // Placeholder for sign-up status
     const [showSignUpModal, setShowSignUpModal] = useState(false); // Modal visibility state
     const [plantData, setPlantData] = useState(null);  // The data recieved from backend that includes recommendations of plant to  be displayed in frontend
     const handleCropRemove = () => {
         setSelectedCrops([]);
       };
+      console.log(setIsSignedUp)
 
       const handleLocationClick = () => {
         if (navigator.geolocation) {
